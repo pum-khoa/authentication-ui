74d93f8dde99d58349af548a82ec705191192482
pumk <dangkhoa.duong@nfq.asia>
2022-05-31 16:55:28 +0700

    feat: add wave background

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Button from './components/Button/Button';
 import { useGlobalData } from './components/GlobalDataProvider/GlobalDataProvider';
 import Loader from './components/Loader/Loader';
+import Wave from './components/Wave/Wave';
 import SignUp from './pages/SignUp/SignUp';
 import { message } from './utils/message';
 
@@ -13,6 +14,7 @@ function App() {
     <>
       <Loader isLoading={ContextData.isLoading} />
       <SignUp />
+      <Wave />
     </>
   );
 }
