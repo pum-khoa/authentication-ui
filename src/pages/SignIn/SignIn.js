35bc3dbead21a3a1421aee6a5961aa6a52bb5cc2
pumk <dangkhoa.duong@nfq.asia>
2022-06-02 14:13:10 +0700

    style: add background image

--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -40,81 +40,83 @@ const SignIn = () => {
   };
 
   return (
-    <div className="sign-up-ctn">
-      <div className="form-title-ctn">
-        <h1>
-          Sign In<span className="dot-icon">.</span>
-        </h1>
-        <p>Welcome back my friend. Now please sign in to use this app.</p>
-      </div>
+    <div className="sign-in-wrapper">
+      <div className="sign-up-ctn">
+        <div className="form-title-ctn">
+          <h1>
+            Sign In<span className="dot-icon">.</span>
+          </h1>
+          <p>Welcome back my friend. Now please sign in to use this app.</p>
+        </div>
 
-      <form
-        className="form-sign-up"
-        id="form-sign-up"
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <Fieldset
-          id="email"
-          type="text"
-          labelName="Email"
-          placeHold="example@email.com"
-          require={true}
-          errorMess={errors.email && errors.email.message}
-          register={{
-            ...register('email', {
-              required: { value: true, message: 'Email is required' },
-              pattern: {
-                value:
-                  // eslint-disable-next-line no-useless-escape
-                  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                message: 'Email is invalid',
-              },
-            }),
-          }}
-        />
-        <Tooltip label="If you forgot your account. Please contact to Admin 😀!">
+        <form
+          className="form-sign-up"
+          id="form-sign-up"
+          onSubmit={handleSubmit(onSubmit)}
+        >
           <Fieldset
-            id="password"
-            type="password"
-            labelName="Password"
-            placeHold="******"
+            id="email"
+            type="text"
+            labelName="Email"
+            placeHold="example@email.com"
             require={true}
-            errorMess={errors.password && errors.password.message}
+            errorMess={errors.email && errors.email.message}
             register={{
-              ...register('password', {
-                required: { value: true, message: 'Password is required' },
-                minLength: {
-                  value: 6,
-                  message: 'Password must have at least 6 characters',
+              ...register('email', {
+                required: { value: true, message: 'Email is required' },
+                pattern: {
+                  value:
+                    // eslint-disable-next-line no-useless-escape
+                    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                  message: 'Email is invalid',
                 },
-                maxLength: 50,
               }),
             }}
           />
-        </Tooltip>
-        <Checkbox
-          name="remember"
-          value="remember"
-          register={{
-            ...register('remember'),
-          }}
-        >
-          Remember me
-        </Checkbox>
-        <Button
-          htmlType="submit"
-          type="primary"
-          block={true}
-          className="button-submit-sign-in"
-        >
-          Submit
-        </Button>
-      </form>
-      <div className="form-footer-wrapper">
-        <p>Not have an account yet?</p>
-        <Link className="sign-in" to="/sign-up">
-          Sign up
-        </Link>
+          <Tooltip label="If you forgot your account. Please contact to Admin 😀!">
+            <Fieldset
+              id="password"
+              type="password"
+              labelName="Password"
+              placeHold="******"
+              require={true}
+              errorMess={errors.password && errors.password.message}
+              register={{
+                ...register('password', {
+                  required: { value: true, message: 'Password is required' },
+                  minLength: {
+                    value: 6,
+                    message: 'Password must have at least 6 characters',
+                  },
+                  maxLength: 50,
+                }),
+              }}
+            />
+          </Tooltip>
+          <Checkbox
+            name="remember"
+            value="remember"
+            register={{
+              ...register('remember'),
+            }}
+          >
+            Remember me
+          </Checkbox>
+          <Button
+            htmlType="submit"
+            type="primary"
+            block={true}
+            className="button-submit-sign-in"
+          >
+            Submit
+          </Button>
+        </form>
+        <div className="form-footer-wrapper">
+          <p>Not have an account yet?</p>
+          <Link className="sign-in" to="/sign-up">
+            Sign up
+          </Link>
+        </div>
       </div>
     </div>
   );
